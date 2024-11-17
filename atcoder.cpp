d36c2a2557387e8fbb32f4fb5ca4a92be330eb4d
Shota Izumi - 和泉将太 <syouta_izumi@post.pioneer.co.jp>
2024-11-17 20:26:36 +0900

    追記.

--- atcoder.cpp
+++ atcoder.cpp
@@ -1,15 +1,13 @@
-#include <iostream>
-#include <sstream>
 #include "atcoder/all"
+#include <bits/stdc++.h>
 
 using Int = int64_t;
-using mint = atcoder::modint998244353;
 #define yes "Yes"
 #define no  "No"
 template<typename T> std::vector<T> getArray(int N, std::istream &is) { std::vector<T> r(N, T{}); for (auto &v : r) { is >> v; } return r; }
 template<typename T> void chmax(T &max, const T &newVal) { max = std::max(max, newVal); }
 template<typename T> void chmin(T &min, const T &newVal) { min = std::min(min, newVal); }
-template<typename T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) { for (auto & val : vec) {os << val << '\n';} return os; }
+template<typename T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& obj) { for (auto & val : obj) {os << val << ' ';} os << std::endl; return os; }
 
 #define handleInput(stream) \
 
--- utility.cpp
+++ utility.cpp
@@ -1,3 +1,21 @@
+Int POW(Int base, int x) {
+	Int result = 1;
+	for (int i = 0; i < x; i++) {
+		result *= base;
+	}
+	return result;
+}
+
+atcoder::modint POW(atcoder::modint base, int x) {
+	mint r = 1; while (x) { if (x & 1) r *= base; base *= base; x >>= 1; } return r;
+}
+
+Int gcd(Int x, Int y) {
+	if (x > y) std::swap(x, y);
+	if (y % x == 0) return x;
+	return gcd(y%x, x);
+}
+
 // デバッグ表示用
 template<typename T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& obj) {
 	for (auto & val : obj) {os << val << ' ';}
@@ -6,9 +24,19 @@ template<typename T> std::ostream& operator<<(std::ostream& os, const std::vecto
 }
 template<typename T> void p(const T &V) {
 	for (auto &v : V) {
-		std::cout << v << " ";
+		std::cerr << v << " ";
 	}
-	std::cout<< std::endl;
+	std::cerr<< std::endl;
+}
+
+template<typename T> std::ostream& operator<<(std::ostream& os, const std::pair<T,T>& obj) { os << obj.first << '/' << obj.second; return os; }
+
+atcoder::modint nCx(int N, int x) {
+    // nCx
+	atcoder::modint res = 1;
+	for (int i = 0; i < x; i++) { res *= N - i; }
+	for (int i = 1; i < x + 1; i++) { res /= i; }
+    return res;
 }
 
 // ↖︎↑↗︎←→↙︎↓↘︎
@@ -18,6 +46,18 @@ std::vector<int> dw = {-1,  0,  1,-1, 1,-1, 0, 1};
 // ↑←→↓
 std::vector<int> dh = {-1, 0, 0, 1};
 std::vector<int> dw = {0, -1, 1, 0};
+
+template<typename T> 
+std::vector<T> rotate(const std::vector<T> &obj) {
+	std::vector<T> result = obj;
+	int N = (int)obj.size();
+	for (int h = 0; h < N; h++) {
+	for (int w = 0; w < N; w++) {
+		result[h][w] = obj[N-1-w][h];
+	}
+	}
+	return result;
+}
 
 // 素数
 std::vector<Int> getPrimes(Int N) {
@@ -55,4 +95,33 @@ std::vector<Int> primeFactorization(Int N) {
 		result.emplace_back(n);
 	}
 	return result;
+}
+
+// ダイクストラ法関連
+struct Edge {
+	Int cost;
+	Int to;
+};
+using Cost = Int;
+using Node = Int;
+using Pair = std::pair<Cost, Node>;
+const Int Inf = 1e18;
+
+{
+	std::vector<Int> dist(N, Inf);
+	std::priority_queue<Pair, std::vector<Pair>, std::greater<Pair>> q;
+
+	q.emplace(0, 0);
+	dist[0] = 0;
+	while (not q.empty()) {
+		auto [cost, from] = q.top(); q.pop();
+		if (dist[from] != cost) continue;
+
+		for (auto &e : graph[from]) {
+			int ncost = e.cost + cost;
+			if (ncost >= dist[e.to]) continue;
+			dist[e.to] = ncost;
+			q.emplace(ncost, e.to);
+		}
+	}
 }
